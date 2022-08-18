916c9a11ea3bc73b7724bbf50d25bf38941792c8
Peter Loomis <loomispeter@gmail.com>
2022-08-18 12:07:44 -0700

    fix polling implementation

---
+++ src/main/java/com/eppo/sdk/helpers/FetchConfigurationsTask.java
@@ -0,0 +1,25 @@
+package com.eppo.sdk.helpers;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class FetchConfigurationsTask extends TimerTask {
+
+  private ConfigurationStore configurationStore;
+  private Timer timer;
+  private long intervalInMillis;
+  private long jitterInMillis;
+
+  @Override
+  public void run() {
+    if (configurationStore.isFetchingExperimentConfigurationAllowed()) {
+      configurationStore.fetchAndSetExperimentConfiguration();
+      long delay = this.intervalInMillis - ((long) Math.random() * this.jitterInMillis);
+      FetchConfigurationsTask nextTask = new FetchConfigurationsTask(configurationStore, timer, intervalInMillis, jitterInMillis);
+      timer.schedule(nextTask, delay);
+    }
+  }
+}
