4f7b91ae9f4855d0ec7a449c87eb2dacc530955a
Simon Dale <33907262+simondale00@users.noreply.github.com>
2024-02-08 11:31:10 -0500

    [EN-17858] FetchConfigurationsTask is more fault tolerant (#5)
    * [EN-17858] FetchConfigurationsTask is more fault tolerant
    
    * revert change to pom.xml, thereby having a single, clean commit that can be merged back to the Eppo main repo

--- src/main/java/com/eppo/sdk/helpers/FetchConfigurationsTask.java
+++ src/main/java/com/eppo/sdk/helpers/FetchConfigurationsTask.java
@@ -4,8 +4,10 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @AllArgsConstructor
+@Slf4j
 public class FetchConfigurationsTask extends TimerTask {
 
   private ConfigurationStore configurationStore;
@@ -15,8 +17,16 @@ public class FetchConfigurationsTask extends TimerTask {
 
   @Override
   public void run() {
-    configurationStore.fetchAndSetExperimentConfiguration();
-    long delay = this.intervalInMillis - ((long) Math.random() * this.jitterInMillis);
+    // Uncaught runtime exceptions will prevent this task from being rescheduled.
+    // As a result, the SDK will continue functioning using the in-memory cache, but will never attempt
+    // to synchronize with Eppo Cloud again.
+    try {
+      configurationStore.fetchAndSetExperimentConfiguration();
+    } catch (Exception e) {
+      log.error("[Eppo SDK] Error fetching experiment configuration", e);
+    }
+
+    long delay = this.intervalInMillis - (long) (Math.random() * this.jitterInMillis);
     FetchConfigurationsTask nextTask = new FetchConfigurationsTask(configurationStore, timer, intervalInMillis, jitterInMillis);
     timer.schedule(nextTask, delay);
   }
