4eb17aee2b6c85bf14289f47473f22d75a781fb6
Aaron Silverman <aaron.silverman@geteppo.com>
2024-08-13 11:40:43 -0600

    logger test added

--- src/test/java/com/eppo/sdk/EppoClientTest.java
+++ src/test/java/com/eppo/sdk/EppoClientTest.java
@@ -4,25 +4,35 @@ import static cloud.eppo.helpers.AssignmentTestCase.parseTestCaseFile;
 import static cloud.eppo.helpers.AssignmentTestCase.runTestCase;
 import static cloud.eppo.helpers.BanditTestCase.parseBanditTestCaseFile;
 import static cloud.eppo.helpers.BanditTestCase.runBanditTestCase;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
 import cloud.eppo.helpers.AssignmentTestCase;
 import cloud.eppo.helpers.BanditTestCase;
+import cloud.eppo.logging.Assignment;
 import cloud.eppo.logging.AssignmentLogger;
+import cloud.eppo.logging.BanditAssignment;
 import cloud.eppo.logging.BanditLogger;
+import cloud.eppo.ufc.dto.Attributes;
+import cloud.eppo.ufc.dto.BanditActions;
+import cloud.eppo.ufc.dto.BanditResult;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
 import java.io.File;
+import java.util.Date;
 import java.util.stream.Stream;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.ArgumentCaptor;
 
 @ExtendWith(WireMockExtension.class)
 public class EppoClientTest {
@@ -103,6 +113,99 @@ public class EppoClientTest {
 
   private static Stream<Arguments> getBanditTestData() {
     return BanditTestCase.getBanditTestData();
+  }
+
+  @SuppressWarnings("ExtractMethodRecommender")
+  @Test
+  public void testLoggers() {
+    Date testStart = new Date();
+    EppoClient eppoClient = initClient(DUMMY_BANDIT_API_KEY);
+
+    String flagKey = "banner_bandit_flag";
+    String subjectKey = "bob";
+    Attributes subjectAttributes = new Attributes();
+    subjectAttributes.put("age", 25);
+    subjectAttributes.put("country", "USA");
+    subjectAttributes.put("gender_identity", "female");
+
+    BanditActions actions = new BanditActions();
+
+    Attributes nikeAttributes = new Attributes();
+    nikeAttributes.put("brand_affinity", 1.5);
+    nikeAttributes.put("loyalty_tier", "silver");
+    actions.put("nike", nikeAttributes);
+
+    Attributes adidasAttributes = new Attributes();
+    adidasAttributes.put("brand_affinity", -1.0);
+    adidasAttributes.put("loyalty_tier", "bronze");
+    actions.put("adidas", adidasAttributes);
+
+    Attributes rebookAttributes = new Attributes();
+    rebookAttributes.put("brand_affinity", 0.5);
+    rebookAttributes.put("loyalty_tier", "gold");
+    actions.put("reebok", rebookAttributes);
+
+    BanditResult banditResult =
+      eppoClient.getBanditAction(flagKey, subjectKey, subjectAttributes, actions, "control");
+
+    // Verify assignment
+    assertEquals("banner_bandit", banditResult.getVariation());
+    assertEquals("adidas", banditResult.getAction());
+
+    Date inTheNearFuture = new Date(System.currentTimeMillis() + 1);
+
+    // Verify experiment assignment log
+    ArgumentCaptor<Assignment> assignmentLogCaptor = ArgumentCaptor.forClass(Assignment.class);
+    verify(mockAssignmentLogger, times(1)).logAssignment(assignmentLogCaptor.capture());
+    Assignment capturedAssignment = assignmentLogCaptor.getValue();
+    assertTrue(capturedAssignment.getTimestamp().after(testStart));
+    assertTrue(capturedAssignment.getTimestamp().before(inTheNearFuture));
+    assertEquals("banner_bandit_flag-training", capturedAssignment.getExperiment());
+    assertEquals(flagKey, capturedAssignment.getFeatureFlag());
+    assertEquals("training", capturedAssignment.getAllocation());
+    assertEquals("banner_bandit", capturedAssignment.getVariation());
+    assertEquals(subjectKey, capturedAssignment.getSubject());
+    assertEquals(subjectAttributes, capturedAssignment.getSubjectAttributes());
+    assertEquals("false", capturedAssignment.getMetaData().get("obfuscated"));
+
+    // Verify bandit log
+    ArgumentCaptor<BanditAssignment> banditLogCaptor =
+      ArgumentCaptor.forClass(BanditAssignment.class);
+    verify(mockBanditLogger, times(1)).logBanditAssignment(banditLogCaptor.capture());
+    BanditAssignment capturedBanditAssignment = banditLogCaptor.getValue();
+    assertTrue(capturedBanditAssignment.getTimestamp().after(testStart));
+    assertTrue(capturedBanditAssignment.getTimestamp().before(inTheNearFuture));
+    assertEquals(flagKey, capturedBanditAssignment.getFeatureFlag());
+    assertEquals("banner_bandit", capturedBanditAssignment.getBandit());
+    assertEquals(subjectKey, capturedBanditAssignment.getSubject());
+    assertEquals("adidas", capturedBanditAssignment.getAction());
+    assertEquals(0.099, capturedBanditAssignment.getActionProbability(), 0.0002);
+    assertEquals("v123", capturedBanditAssignment.getModelVersion());
+
+    Attributes expectedSubjectNumericAttributes = new Attributes();
+    expectedSubjectNumericAttributes.put("age", 25);
+    assertEquals(
+      expectedSubjectNumericAttributes, capturedBanditAssignment.getSubjectNumericAttributes());
+
+    Attributes expectedSubjectCategoricalAttributes = new Attributes();
+    expectedSubjectCategoricalAttributes.put("country", "USA");
+    expectedSubjectCategoricalAttributes.put("gender_identity", "female");
+    assertEquals(
+      expectedSubjectCategoricalAttributes,
+      capturedBanditAssignment.getSubjectCategoricalAttributes());
+
+    Attributes expectedActionNumericAttributes = new Attributes();
+    expectedActionNumericAttributes.put("brand_affinity", -1.0);
+    assertEquals(
+      expectedActionNumericAttributes, capturedBanditAssignment.getActionNumericAttributes());
+
+    Attributes expectedActionCategoricalAttributes = new Attributes();
+    expectedActionCategoricalAttributes.put("loyalty_tier", "bronze");
+    assertEquals(
+      expectedActionCategoricalAttributes,
+      capturedBanditAssignment.getActionCategoricalAttributes());
+
+    assertEquals("false", capturedBanditAssignment.getMetaData().get("obfuscated"));
   }
 
   private EppoClient initClient(String apiKey) {
